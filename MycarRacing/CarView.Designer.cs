25f1b7db2824e6ab9516482ca700d6b0e5bcd9fa
Iva <rybnikovv@list.ru>
2020-09-20 22:25:20 +0400

    firs v1

---
+++ MycarRacing/CarView.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace MycarRacing
+{
+    partial class CarView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tmrRacing = new System.Windows.Forms.Timer(this.components);
+            this.Score_txt = new System.Windows.Forms.Label();
+            this.High_txt = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tmrRacing
+            // 
+            this.tmrRacing.Enabled = true;
+            this.tmrRacing.Interval = 50;
+            this.tmrRacing.Tick += new System.EventHandler(this.tmrRacing_Tick);
+            // 
+            // Score_txt
+            // 
+            this.Score_txt.AutoSize = true;
+            this.Score_txt.Location = new System.Drawing.Point(350, 70);
+            this.Score_txt.Name = "Score_txt";
+            this.Score_txt.Size = new System.Drawing.Size(45, 17);
+            this.Score_txt.TabIndex = 0;
+            this.Score_txt.Text = "Score";
+            // 
+            // High_txt
+            // 
+            this.High_txt.AutoSize = true;
+            this.High_txt.Location = new System.Drawing.Point(333, 107);
+            this.High_txt.Name = "High_txt";
+            this.High_txt.Size = new System.Drawing.Size(78, 17);
+            this.High_txt.TabIndex = 2;
+            this.High_txt.Text = "High Score";
+            // 
+            // CarView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(535, 615);
+            this.Controls.Add(this.High_txt);
+            this.Controls.Add(this.Score_txt);
+            this.DoubleBuffered = true;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "CarView";
+            this.Text = "Car Racing";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.CarView_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.CarView_KeyDown);
+            this.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.CarView_MouseDoubleClick);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer tmrRacing;
+        private System.Windows.Forms.Label Score_txt;
+        private System.Windows.Forms.Label High_txt;
+    }
+}
+
---
+++ MycarRacing/CarView.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Media;
+
+namespace MycarRacing
+{
+    public partial class CarView : Form
+    {
+        #region Fields
+
+        private int col;
+
+        private int elementSize;
+        private int[,] gameMatrix;
+        private int row;
+        //the start point of board
+        private int startX;
+        private int startY;
+        //the position of car in time
+        private int carX;
+        private int carY;
+
+        private Random random;
+        private int myCarPosition;
+
+        private int LeftWallX;
+        private int LeftWallY;
+
+        private int RightWallX;
+        private int RightWallY;
+
+        private int MyScore;
+        private int HighScore;
+        #endregion
+
+        public CarView()
+        {
+            InitializeComponent();
+            InitializeGame();
+            game.Play();
+        }
+
+        private void InitializeGame()
+        {           
+            row = 20;
+            col = 10;
+            startX = 50;
+            startY = 50;
+            elementSize = 15;
+
+            carX = carY = 2;
+            gameMatrix = new int[row, col];
+            ResetGameBoard();
+
+            random = new Random();
+            myCarPosition = 2;
+
+            LeftWallX = 1;
+            LeftWallY = 0;
+
+            RightWallX = 1;
+            RightWallY = 9;
+
+            MyScore = 0;
+            HighScore = 0;
+        }
+
+        private void CarView_Paint(object sender, PaintEventArgs e)
+        {
+            DrawGameBoard(e.Graphics);
+        }
+
+        SoundPlayer game = new SoundPlayer(@"C:\Users\пк\Desktop\Music\МузыкаИгры.wav");
+
+        SoundPlayer crash = new SoundPlayer(@"C:\Users\пк\Desktop\Music\МузыкаСтолкновения.wav");
+
+        private void DrawGameBoard(Graphics g)
+        {
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < col; j++)
+                {
+                    g.DrawRectangle(new Pen(Brushes.Black), startX + j * elementSize,
+                                    startY + i * elementSize, elementSize, elementSize);
+                    if (gameMatrix[i, j] == 1)
+                    {
+                        g.FillRectangle(Brushes.Gray, startX + j * elementSize,
+                                        startY + i * elementSize, elementSize, elementSize);
+                    }
+                    if (gameMatrix[i, j] == 2)
+                    {
+                        g.FillRectangle(Brushes.Gray, startX + j * elementSize,
+                                        startY + i * elementSize, elementSize, elementSize);
+                    }
+                }
+            }
+        }
+
+        #region Functions
+
+        private void ResetGameBoard()
+        {
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < col; j++)
+                {
+                    gameMatrix[i, j] = 0;
+                }
+            }
+        }
+
+        private void DrawACar(int x, int y, int value)
+        {
+            DrawAPoint(x, y + 1, value);
+            DrawAPoint(x + 1, y + 1, value);
+            DrawAPoint(x + 2, y + 1, value);
+            DrawAPoint(x + 1, y, value);
+            DrawAPoint(x + 1, y + 2, value);
+            DrawAPoint(x + 3, y, value);
+            DrawAPoint(x + 3, y + 2, value);
+        }
+
+        private void DrawABarrier(int x, int y, int value)
+        {
+            DrawAPoint(x, y, value);
+            DrawAPoint(x + 2, y, value);
+            DrawAPoint(x + 1, y + 1, value);
+            DrawAPoint(x + 2, y + 1, value);
+            DrawAPoint(x + 3, y + 1, value);
+            DrawAPoint(x, y + 2, value);
+            DrawAPoint(x + 2, y + 2, value);
+        }
+        
+        private void DrawLeftWall(int x, int y, int value)
+        {
+            for (int k = 0; k < row; k++)                
+            {
+                DrawAPoint(x, y, value);
+                x++;
+                if (x % 4 == 0)
+                {
+                    x++;
+                }
+            }
+        }
+        private void DrawRightWall(int x, int y, int value)
+        {
+            for (int t = 0; t < row; t++)
+            {
+                DrawAPoint(x, y, value);
+                x++;
+                if (x % 4 == 0)
+                {
+                    x++;
+                }
+            }
+        }
+
+        private void DrawAPoint(int x, int y, int value)
+        {
+            if (x < row && x >= 0 && y < col && y >= 0)
+            {
+                gameMatrix[x, y] = value;
+            }
+        }
+
+        #endregion
+
+        private void tmrRacing_Tick(object sender, EventArgs e)
+        {
+            ResetGameBoard();
+            DrawACar(16, myCarPosition, 2);
+            DrawABarrier(carX, carY, 2);
+            DrawLeftWall(LeftWallX, LeftWallY, 2);
+            DrawRightWall(RightWallX, RightWallY, 2);
+            Invalidate();
+
+            carX++;
+            if (carX == row)
+            {
+                carX = 0;
+                carY = random.Next() % 2 == 0 ? 2 : 5;
+            }
+            CheckGame();
+            MyScore++;
+            Score_txt.Text = "Score - " + MyScore;
+        }
+
+        private void CheckGame()
+        {           
+            if (carX + 3 > 16 && carY == myCarPosition)
+            {
+                crash.Play();
+                tmrRacing.Enabled = false;
+            }
+        }
+
+        private void CarView_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left && myCarPosition == 5)
+            {
+                myCarPosition = 2;
+            }
+            else if (e.KeyCode == Keys.Right && myCarPosition == 2)
+            {
+                myCarPosition = 5;
+            }
+        }
+
+        private void CarView_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            carX = carY = 2;
+            myCarPosition = 2;
+            tmrRacing.Enabled = true;
+            if (MyScore > HighScore)
+            {
+                HighScore = MyScore;
+            }
+            High_txt.Text = "HighScore - " + HighScore;
+            MyScore = 0;
+            game.Play();
+        }
+
+       
+
+    }
+}
